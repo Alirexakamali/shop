4b8ff9349b734a34956751345965f1437f5d76b1
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:38:43 +0330

    feat(admin): implement discounts admin interface

--- apps/discounts/admin.py
+++ apps/discounts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Discount, UserDiscount
+
+
+class UserDiscountInline(admin.TabularInline):
+    model = UserDiscount
+    extra = 0
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    inlines = [UserDiscountInline]
+
+    list_display = (
+        "code",
+        "discount_type",
+        "value",
+        "used_count",
+        "is_active",
+    )
+
+    list_filter = (
+        "discount_type",
+        "is_active",
+    )
+
+    search_fields = (
+        "code",
+    )
+
+
+@admin.register(UserDiscount)
+class UserDiscountAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "discount",
+        "used",
+        "used_at",
+    )
+
+    autocomplete_fields = (
+        "user",
+        "discount",
+    )
