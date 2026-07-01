1e143c1c43d18e4567ac631c499870b9e3a6aa52
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 16:28:36 +0330

    feat(auth): implement login serializer

---
+++ api/v1/accounts/serializers.py
@@ -0,0 +1,33 @@
+from django.core.validators import validate_email
+from rest_framework import serializers
+
+from apps.accounts.validators import validate_phone_number
+
+
+class LoginSerializer(serializers.Serializer):
+    identifier = serializers.CharField(max_length=255)
+
+    def validate_identifier(self, value):
+        value = value.strip()
+
+        try:
+            validate_email(value)
+            return {
+                "type": "email",
+                "value": value.lower(),
+            }
+        except Exception:
+            pass
+
+        try:
+            validate_phone_number(value)
+            return {
+                "type": "phone",
+                "value": value,
+            }
+        except Exception:
+            pass
+
+        raise serializers.ValidationError(
+            "Enter a valid email or phone number."
+        )
