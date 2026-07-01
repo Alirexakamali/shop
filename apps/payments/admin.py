95894091b6eee46d769c1afcd6875f30c3b474c6
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:37:56 +0330

    feat(admin): implement payments admin interface

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "amount",
+        "method",
+        "status",
+        "paid_at",
+    )
+
+    list_filter = (
+        "status",
+        "method",
+    )
+
+    search_fields = (
+        "authority",
+        "transaction_id",
+        "order__id",
+    )
+
+    autocomplete_fields = (
+        "order",
+    )
+
+    ordering = (
+        "-created_at",
+    )
