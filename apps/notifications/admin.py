cad41307b3ee8be95ad0a07c15dc4d557c55258b
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:39:53 +0330

    feat(admin): implement notifications admin interface

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "user",
+        "notification_type",
+        "is_read",
+        "created_at",
+    )
+
+    list_filter = (
+        "notification_type",
+        "is_read",
+    )
+
+    autocomplete_fields = (
+        "user",
+    )
+
+    search_fields = (
+        "title",
+        "user__phone",
+    )
