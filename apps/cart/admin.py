19d516e5c1a7dc47dfd7da20d04139a0d5b2f60f
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:36:39 +0330

    feat(admin): implement cart admin interface

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+    )
+
+    autocomplete_fields = (
+        "user",
+    )
+
+    search_fields = (
+        "user__phone",
+        "user__email",
+    )
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "cart",
+        "variant",
+        "quantity",
+    )
+
+    autocomplete_fields = (
+        "cart",
+        "variant",
+    )
