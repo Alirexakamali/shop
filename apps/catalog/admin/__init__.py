31ccc0e8bc7fbefb5db7e9137f106fe1480cf9f4
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:12:20 +0330

    feat(admin): implement category, brand and attribute admin interfaces

---
+++ apps/catalog/admin/__init__.py
@@ -0,0 +1,11 @@
+from .attribute import Attribute
+from .attribute_value import AttributeValue
+from .brand import Brand
+from .category import Category
+
+__all__ = [
+    "Attribute",
+    "AttributeValue",
+    "Brand",
+    "Category",
+]
