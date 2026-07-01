e76675a99eeae54afd08e28dfc0ed61957c129a0
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:38:18 +0330

    feat(admin): implement shipping admin interface

--- apps/shipping/admin.py
+++ apps/shipping/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shipment, ShippingMethod
+
+
+@admin.register(ShippingMethod)
+class ShippingMethodAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+        "estimated_days",
+        "is_active",
+    )
+
+    list_editable = (
+        "price",
+        "is_active",
+    )
+
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "shipping_method",
+        "tracking_code",
+        "status",
+    )
+
+    list_filter = (
+        "status",
+        "shipping_method",
+    )
+
+    autocomplete_fields = (
+        "order",
+        "shipping_method",
+    )
+
+    search_fields = (
+        "tracking_code",
+    )
