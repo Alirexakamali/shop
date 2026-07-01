1397a036e48fc180cb2d30428b0ce4b993951ce2
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:39:26 +0330

    feat(admin): implement reviews admin interface

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "user",
+        "rating",
+        "is_approved",
+        "created_at",
+    )
+
+    list_filter = (
+        "rating",
+        "is_approved",
+    )
+
+    list_editable = (
+        "is_approved",
+    )
+
+    autocomplete_fields = (
+        "user",
+        "product",
+    )
+
+    search_fields = (
+        "product__name",
+        "user__phone",
+    )
