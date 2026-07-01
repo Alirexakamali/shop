790ea08a11dda368ebda2a91fdf6d0d7802432a0
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:40:21 +0330

    feat(admin): implement cms admin interface

--- apps/cms/admin.py
+++ apps/cms/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Banner, Page
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "is_published",
+    )
+
+    list_editable = (
+        "is_published",
+    )
+
+    search_fields = (
+        "title",
+        "slug",
+    )
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "position",
+        "is_active",
+    )
+
+    list_filter = (
+        "position",
+        "is_active",
+    )
+
+    list_editable = (
+        "is_active",
+    )
+
+    search_fields = (
+        "title",
+    )
