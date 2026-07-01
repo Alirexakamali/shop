2bc83d2ee2526330c444bd4765a2482b021ef3c8
AlirexaKamali <alirexakamali@gmail.com>
2026-07-01 14:37:06 +0330

    feat(admin): implement orders admin interface

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+    list_display = (
+        "id",
+        "user",
+        "status",
+        "total_amount",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    autocomplete_fields = (
+        "user",
+        "address",
+    )
+
+    search_fields = (
+        "user__phone",
+        "user__email",
+    )
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "order",
+        "variant",
+        "quantity",
+        "total_price",
+    )
+
+    autocomplete_fields = (
+        "order",
+        "variant",
+    )
