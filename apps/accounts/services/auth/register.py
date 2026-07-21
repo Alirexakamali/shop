adebe959977eff464555e79447662b6eec72c518
AlirexaKamali <alirexakamali@gmail.com>
2026-07-21 15:40:18 +0330

    feat(accounts): implement register service

--- apps/accounts/services/auth/register.py
+++ apps/accounts/services/auth/register.py
@@ -1,14 +1,31 @@
-from attrs import define, field
+from django.contrib.auth.hashers import make_password
 
-
-@define(frozen=True, slots=True)
-class RegisterData:
-    first_name: str
-    last_name: str
-    email: str = field(converter=str.lower)
-    password: str
+from ...dto.register import RegisterDTO
+from ...enums.register import RegisterStatus
+from ...selectors import PendingRegistrationSelector, UserSelector
+from ...services.auth.otp import OTPService
 
 
 class RegisterService:
-    
-    def register(self,data: RegisterData) -> None: ...
+    @staticmethod
+    def register(*, data: RegisterDTO) -> RegisterStatus:
+        if UserSelector.exists_by_email(email=data.email):
+            return RegisterStatus.USER_ALREADY_EXISTS
+
+        pending = PendingRegistrationSelector.get_by_email(
+            email=data.email,
+        )
+
+        otp = OTPService.generate()
+
+        hashed_password = make_password(data.password)
+
+        if pending:
+            # update pending registration
+            ...
+
+        else:
+            # create pending registration
+            ...
+
+        return {"status": RegisterStatus.OTP_SENT}
