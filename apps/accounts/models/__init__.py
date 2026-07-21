adafcbd15cad9f35fb9110c5fe24d7b87f0e489e
AlirexaKamali <alirexakamali@gmail.com>
2026-07-21 15:39:48 +0330

    feat(accounts): add pending registration model

--- apps/accounts/models/__init__.py
+++ apps/accounts/models/__init__.py
@@ -1,7 +1,8 @@
 from .user import User
 from .address import Address 
-
+from .pending_registration import PendingRegistration
 __all__ = [
     "User",
     "Address",
+    "PendingRegistration",
 ]
